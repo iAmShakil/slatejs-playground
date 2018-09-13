3c3ebfeb9c8eec88c6e79d626788648733298ca4
Shakil <shakilofficial7@gmail.com>
2018-09-13 16:27:17 +0600

    image highlights on select

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,14 @@
 import React from 'react'
-
 class Image extends React.Component {
   state = {}
-
+  
   componentDidMount() {
     const { node } = this.props
     const { data } = node
     const file = data.get('file')
     this.load(file)
   }
-
+  
   load(file) {
     const reader = new FileReader()
     reader.addEventListener('load', () => this.setState({ src: reader.result }))
@@ -20,7 +19,17 @@ class Image extends React.Component {
     const { attributes } = this.props
     const { src } = this.state
     const { children } = this.props
-    return src ? <img {...attributes} src={src} />: <span {...attributes}> Loading... {children} </span>
+    const style = this.props.isFocused? styles.blueBorder : styles.noBorder
+    return src ? <img style={style} {...attributes}  src={src} />: <span {...attributes}> Loading... {children} </span>
+  }
+}
+
+const styles = {
+  blueBorder: {
+    border: '1px solid blue'
+  },
+  noBorder: {
+    border: '0'
   }
 }
 
